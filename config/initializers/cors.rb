ae1e84b4b56cc10a09d9b71267f9a0a9c91eac43
Sam Wagner <sfraserwagner@gmail.com>
2023-03-07 17:13:01 -0800

    Add frontend cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3002'
+    origins 'http://localhost:3002', 'https://run-chat-frontend.vercel.app'
     resource '*', headers: :any, methods: [:get, :post], credentials: true
   end
 end
