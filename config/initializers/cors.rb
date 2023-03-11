abb898f22961cf835afa9f0b3b18b02dff3e43ed
Sam Wagner <sfraserwagner@gmail.com>
2023-03-11 13:13:55 -0800

    Update cors urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3002', 'https://run-chat-frontend.vercel.app'
+    origins 'http://localhost:3002', 'https://runchat.net', 'https://www.runchat.net'
     resource '*', headers: :any, methods: [:get, :post], credentials: true
   end
 end
